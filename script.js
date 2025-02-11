f1ba5c6a348c81d462f1852fd83e88e6e1d9f982
Omm Prakash Samal <ommprakashsamal9@gmail.com>
2025-02-12 01:19:45 +0530

    script.js

--- script.js
+++ script.js
@@ -1,73 +1,86 @@
-var tl = gsap.timeline({scrollTrigger:{
-    trigger: ".two",
-    start: "0% 95%",
-    end: "70% 50%",
-    scrub: true,  
-    // markers: true,
-}})
+// Initialize GSAP Timeline for the first section
+var tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".two",
+        start: "0% 95%",
+        end: "70% 50%",
+        scrub: 0.5,  
+    }
+});
 
-tl.to("#thumsup",{
+// Animations for the first section
+tl.to("#thumsup", {
     top: "120%",
-    left: "0%"
-}, 'orange')
-tl.to("#orange-cut",{
-    top:"160%",
-    left: "23%"
-}, 'orange')
-tl.to("#orange",{
+    left: "0%",
+    ease: "power2.out"
+}, 'orange');
+
+tl.to("#leaf1", {
+    top: "160%",
+    left: "23%",
+    ease: "power2.out"
+}, 'orange');
+
+tl.to("#iceleaf", {
     width: "15%",
-    top:"160%",
-    right: "10%"
-}, 'orange')
-tl.to("#leaf",{
-    top:"110%",
+    top: "160%",
+    right: "10%",
+    ease: "power2.out"
+}, 'orange');
+
+tl.to("#icedrop", {
+    top: "100%",
     rotate: "130deg",
-    left: "70%"
-}, 'orange')
-tl.to("#leaf2",{
-    top:"110%",
-    rotate: "130deg",
-    left: "0%"
-}, 'orange')
+    left: "70%",
+    ease: "power2.out"
+}, 'orange');
 
-
-var tl2 = gsap.timeline({scrollTrigger:{
-    trigger: ".three",
-    start: "0% 95%",
-    end: "20% 50%",
-    scrub: true,
-    // markers: true,
-}})
-
-tl2.from(".lemon1",{
-    rotate: "-90deg",
-    left: "-100%",
-    top: "110%"
-}, 'ca')
-tl2.from("#cocacola",{
-    rotate: "-90deg",
+tl.to("#cocacolalogo", {
     top: "110%",
-    left: "-100%",
-}, 'ca')
-
-tl2.from(".lemon2",{
     rotate: "90deg",
-    left: "100%",
-    top: "110%"
-}, 'ca')
-tl2.from("#pepsi",{
-    rotate: "90deg",
-    top: "110%",
-    left: "100%",
-}, 'ca')
+    left: "0%",
+    ease: "power2.out"
+}, 'orange');
 
-tl2.to("#orange-cut",{
-    width:"18%",
-    left: "42%",
-    top: "204%"
-}, 'ca')
-tl2.to("#thumsup",{
-    width:"25%",
+
+// Initialize GSAP Timeline for the second section
+var tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".three",
+        start: "0% 95%",
+        end: "20% 50%",
+        scrub: 0.5,  
+    }
+});
+
+// Adjusted animation for Coca-Cola can
+tl2.from("#cocacola", {
+    rotate: "-90deg",
+    top: "120%",  // Adjusted top position
+    left: "-10%",  // Moved slightly inward
+    ease: "power2.out"
+}, 'ca');
+
+// Adjusted animation for Pepsi can
+tl2.from("#pepsi", {
+    rotate: "90deg",
+    top: "120%",  // Adjusted top position
+    left: "85%",  // Moved slightly inward
+    ease: "power2.out"
+}, 'ca');
+
+
+tl2.to("#leaf1", {
+    rotate: "175deg",
+    width: "30%",
+    left: "48%",
+    top: "190%",
+    ease: "power2.out"
+}, 'ca');
+
+tl2.to("#thumsup", {
+    width: "25%",
     top: "200%",
     left: "37%",
-}, 'ca')
+    ease: "power2.out"
+}, 'ca');
